b71cddc553f175b6efecd5785047dcde06f53f42
Imad Al-Noumairi <imad@alnoumairi.de>
2024-10-31 06:52:29 +0100

    run poe pre-commit

--- alembic/versions/2024_10_31_0408-92dd17d5ba45_add_description_to_skills_sub_skill.py
+++ alembic/versions/2024_10_31_0408-92dd17d5ba45_add_description_to_skills_sub_skill.py
@@ -10,15 +10,15 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '92dd17d5ba45'
-down_revision = 'e976176560c2'
+revision = "92dd17d5ba45"
+down_revision = "e976176560c2"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column('skills_sub_skill', sa.Column('description', sa.Text, nullable=True))
+    op.add_column("skills_sub_skill", sa.Column("description", sa.Text, nullable=True))
 
 
 def downgrade() -> None:
-    op.drop_column('skills_sub_skill', 'description')
+    op.drop_column("skills_sub_skill", "description")
--- alembic/versions/2024_10_31_0408-e976176560c2_add_description_to_skills_root_skill.py
+++ alembic/versions/2024_10_31_0408-e976176560c2_add_description_to_skills_root_skill.py
@@ -10,15 +10,15 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e976176560c2'
-down_revision = '1a9e957978e2'
+revision = "e976176560c2"
+down_revision = "1a9e957978e2"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column('skills_root_skill', sa.Column('description', sa.Text, nullable=True))
+    op.add_column("skills_root_skill", sa.Column("description", sa.Text, nullable=True))
 
 
 def downgrade() -> None:
-    op.drop_column('skills_root_skill', 'description')
+    op.drop_column("skills_root_skill", "description")
